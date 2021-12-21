97cd6aca20d0c4597992d37e51752918cea0c74f
MYuPopov <beemax@bk.ru>
2021-12-21 16:44:51 +0300

    Первый код

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-console.log("start");
+"use strict";
+
+const currentElementBtnAll = document.querySelector(".current-column");
+console.log("~ currentElementBtnAll", currentElementBtnAll);
+const nextElementBtnAll = document.querySelector(".next-column");
+console.log("~ nextElementBtnAll", nextElementBtnAll);
+
+nextElementBtnAll.addEventListener("click", (event) => {
+	console.log(event.target);
+});
+
+const renderCurrentItems = () => {
+	currentElementBtnAll.innerHTML = "";
+};
